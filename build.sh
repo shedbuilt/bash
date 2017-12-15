93add0ba8cb66431314232e35e4d2c620f36df4f
Auston Stewart <auston@shedbuilt.net>
2017-12-15 10:17:36 -1000

    Initial commit of bash

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+patch -Np1 -i $SHED_PATCHDIR/bash-4.4-upstream_fixes-1.patch
+./configure --prefix=/usr \
+            --docdir=/usr/share/doc/bash-4.4 \
+            --without-bash-malloc \
+            --with-installed-readline
+make -j $SHED_NUMJOBS
+make DESTDIR=${SHED_FAKEROOT} install
+mkdir -v ${SHED_FAKEROOT}/bin
+mv -vf ${SHED_FAKEROOT}/usr/bin/bash ${SHED_FAKEROOT}/bin
