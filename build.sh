819bccfec6734207a95038967ffad6fa833c3606
Auston Stewart <auston@shedbuilt.net>
2017-12-16 17:10:39 -1000

    Updating packaging and adding /bin/sh symlink

--- build.sh
+++ build.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-patch -Np1 -i $SHED_PATCHDIR/bash-4.4-upstream_fixes-1.patch
+patch -Np1 -i "$SHED_PATCHDIR/bash-4.4-upstream_fixes-1.patch"
 ./configure --prefix=/usr \
             --docdir=/usr/share/doc/bash-4.4 \
             --without-bash-malloc \
             --with-installed-readline
 make -j $SHED_NUMJOBS
-make DESTDIR=${SHED_FAKEROOT} install
-mkdir -v ${SHED_FAKEROOT}/bin
-mv -vf ${SHED_FAKEROOT}/usr/bin/bash ${SHED_FAKEROOT}/bin
+make "DESTDIR=${SHED_FAKEROOT}" install
+mkdir -v "${SHED_FAKEROOT}/bin"
+mv -vf "${SHED_FAKEROOT}/usr/bin/bash" "${SHED_FAKEROOT}/bin"
+ln -sv bash "${SHED_FAKEROOT}/bin/sh"
