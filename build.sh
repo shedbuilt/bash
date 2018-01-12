9d0a7b4e923efa8a6e68c5f60e875d60c622c961
Auston Stewart <auston@shedbuilt.net>
2018-01-11 20:59:48 -0500

    Adding toolchain build mode.

--- build.sh
+++ build.sh
@@ -1,10 +1,26 @@
 #!/bin/bash
-./configure --prefix=/usr \
-            --docdir=/usr/share/doc/bash-4.4.12 \
-            --without-bash-malloc \
-            --with-installed-readline
+case "$SHED_BUILDMODE" in
+    toolchain)
+        ./configure --prefix=/tools \
+                    --without-bash-malloc || return 1
+        ;;
+    *)
+        ./configure --prefix=/usr \
+                    --docdir=/usr/share/doc/bash-4.4.12 \
+                    --without-bash-malloc \
+                    --with-installed-readline || return 1
+        ;;
+esac
 make -j $SHED_NUMJOBS
-make "DESTDIR=${SHED_FAKEROOT}" install
-mkdir -v "${SHED_FAKEROOT}/bin"
-mv -vf "${SHED_FAKEROOT}/usr/bin/bash" "${SHED_FAKEROOT}/bin"
-ln -sv bash "${SHED_FAKEROOT}/bin/sh"
+make DESTDIR="$SHED_FAKEROOT" install
+
+case "$SHED_BUILDMODE" in
+    toolchain)
+        ln -sv bash "${SHED_FAKEROOT}/tools/bin/sh"
+        ;;
+    *)    
+        mkdir -v "${SHED_FAKEROOT}/bin"
+        mv -vf "${SHED_FAKEROOT}/usr/bin/bash" "${SHED_FAKEROOT}/bin"
+        ln -sv bash "${SHED_FAKEROOT}/bin/sh"
+        ;;
+esac
